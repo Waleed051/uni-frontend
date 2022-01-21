f066489a6ad15726b7d8ca91a9358f7355dfeeb9
Huzaifa181 <huzaifa.ahmed181@gmail.com>
2022-01-21 22:02:48 +0500

    CHanges

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,36 +1,44 @@
-import axios from 'axios'
-import * as actions from '../constants/productConstants'
+import axios from "axios";
+import * as actions from "../constants/productConstants";
 
-export const listProducts=()=>async dispatch=>{
-    try{
-        dispatch({type:actions.PRODUCT_LIST_REQUEST})
-        const {data} = await axios.get('/api/products')
-        dispatch({
-            type:actions.PRODUCT_LIST_SUCCESS,
-            payload:data
-        })
-    }
-    catch(err){
-        dispatch({
-            type:actions.PRODUCT_LIST_FAIL,
-            payload:err.response && err.response.data.message ? err.response.data.message:err.message,
-        })
-    }
-}
+export const listProducts = () => async (dispatch) => {
+  try {
+    dispatch({ type: actions.PRODUCT_LIST_REQUEST });
+    const { data } = await axios.get(
+      "https://sleepy-plains-69712.herokuapp.com/api/products"
+    );
+    dispatch({
+      type: actions.PRODUCT_LIST_SUCCESS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: actions.PRODUCT_LIST_FAIL,
+      payload:
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message,
+    });
+  }
+};
 
-export const listProductDetails=(id)=>async dispatch=>{
-    try{
-        dispatch({type:actions.PRODUCT_DETAIL_REQUEST})
-        const {data} = await axios.get(`/api/products/${id}`)
-        dispatch({
-            type:actions.PRODUCT_DETAIL_SUCCESS,
-            payload:data
-        })
-    }
-    catch(err){
-        dispatch({
-            type:actions.PRODUCT_DETAIL_FAIL,
-            payload:err.response && err.response.data.message ? err.response.data.message:err.message,
-        })
-    }
-}
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: actions.PRODUCT_DETAIL_REQUEST });
+    const { data } = await axios.get(
+      `https://sleepy-plains-69712.herokuapp.com/api/products/${id}`
+    );
+    dispatch({
+      type: actions.PRODUCT_DETAIL_SUCCESS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: actions.PRODUCT_DETAIL_FAIL,
+      payload:
+        err.response && err.response.data.message
+          ? err.response.data.message
+          : err.message,
+    });
+  }
+};
